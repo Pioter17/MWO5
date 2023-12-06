9010d8c09601ebf109066bb50b48cd6c856d3b12
Pioter17 <piotrwisnie@wp.pl>
2023-12-06 15:39:22 +0100

    one directory

--- frontend/src/environments/environment.stage.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://api.stage.com',
-  configurationName: 'Stage Configuration'
-};
--- frontend/src/environments/environment.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: false,
-  apiUrl: 'https://example.com/api',
-  configurationName: 'Default Configuration',
-};
